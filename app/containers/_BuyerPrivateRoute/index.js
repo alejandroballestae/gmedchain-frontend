5937045885714629ff14500ab8635c6eb1e3ef58
Alejandro <alejandroballestae@gmail.com>
2020-09-07 22:06:22 -0500

    todoW

--- app/containers/_BuyerPrivateRoute/index.js
+++ app/containers/_BuyerPrivateRoute/index.js
@@ -3,10 +3,10 @@ import { Route, Redirect} from 'react-router-dom';
 
 const BuyerPrivateRoute = ({ component: Component, ...rest }) => (
     
-    <Route {...rest} render={(props) => (
+    <Route {...rest} render={(props) => 
         localStorage.getItem('token') && localStorage.getItem('token').length >10 && localStorage.getItem('type') == 'Buyer'
         ?  <Component {...props} />
         :  <Redirect to='/login' />
-    )} />
+    } />
   )
   export default BuyerPrivateRoute;
