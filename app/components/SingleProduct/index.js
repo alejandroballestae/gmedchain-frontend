d8f41bb0ff4653387dbab7772ee2ea5ece5e35c7
Alejandro <alejandroballestae@gmail.com>
2020-09-09 08:04:25 -0500

    changes

--- app/components/SingleProduct/index.js
+++ app/components/SingleProduct/index.js
@@ -13,7 +13,6 @@ const SingleProduct = ({ name, badge, description, image, author, review, price,
     try{
         img_src = require(`../../images/products/${img}`);
     }catch(e){
-        alert(e);
         img_src = require(`../../images/products/img2.jpg`);
     }
     console.log("--------------------------------------xxxxx");
