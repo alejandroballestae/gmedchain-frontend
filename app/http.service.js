b32044163e9abf0a38067de159761838c246ccce
Alejandro <alejandroballestae@gmail.com>
2020-09-09 06:48:33 -0500

    httpservice

--- app/http.service.js
+++ app/http.service.js
@@ -7,7 +7,7 @@ const config = {
 axios.defaults.headers = config.headers;
 */
 const instance =  axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://0.0.0.0:5000",
   responseType: "json"
 });
 
